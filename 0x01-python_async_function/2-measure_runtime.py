8479e4db89a3b60fa25ed7f047650d0a3eb8502f
Tatenda Katema <tkatema5@gmail.com>
2023-01-09 15:32:23 +0200

    submitting

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -3,7 +3,7 @@
 
 from time import perf_counter
 import asyncio
-wait_n = __import__('1_concurrent_coroutines').wait_n
+wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
